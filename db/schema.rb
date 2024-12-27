d0b553ec270480eaf644fbcb54d9f568c58a6c11
Yusuke Sangenya <longinus.eva@gmail.com>
2024-12-27 15:14:01 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_27_055709) do
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name"
+  end
 end
