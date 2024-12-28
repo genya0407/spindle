ec9c8241be24c86dc3069583556d9032e665dd54
Yusuke Sangenya <longinus.eva@gmail.com>
2024-12-28 17:04:52 +0900

    fix

--- db/migrate/20241228075922_add_inbox_to_account.rb
+++ db/migrate/20241228075922_add_inbox_to_account.rb
@@ -1,6 +1,7 @@
 class AddInboxToAccount < ActiveRecord::Migration[8.0]
   def change
     RemoteAccount.delete_all
+    Followership.delete_all
     add_column :remote_accounts, :inbox, :text, null: false
   end
 end
