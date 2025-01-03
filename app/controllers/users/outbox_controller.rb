6e01bef6686ab1e220cce8ed4b6a8967bd5d5874
Yusuke Sangenya <longinus.eva@gmail.com>
2025-01-03 17:27:02 +0900

    outbox

--- app/controllers/users/outbox_controller.rb
+++ app/controllers/users/outbox_controller.rb
@@ -1,11 +1,27 @@
 class Users::OutboxController < ApplicationController
-  before_action :require_actor_signature!
-
   before_action do
     head :not_found unless target_group
   end
 
-  def create
+  def show
+    json = {
+      "@context": "https://www.w3.org/ns/activitystreams",
+      id: outbox_user_url(id: target_group.name),
+      type: "OrderedCollection",
+      totalItems: target_group.boosts.count,
+      orderedItems: target_group.boosts.order(id: :desc).limit(100).map do |boost|
+        {
+          "@context": "https://www.w3.org/ns/activitystreams",
+          id: boost.uri,
+          to: "https://www.w3.org/ns/activitystreams#Public",
+          cc: [ boost.actor.followers_uri ],
+          type: "Announce",
+          actor: boost.actor.uri,
+          object: boost.original_status_uri
+        }
+      end
+    }
+    render json: json
   end
 
   private
